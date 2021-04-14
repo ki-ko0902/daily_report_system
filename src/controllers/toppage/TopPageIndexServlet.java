43e07ea2c9b2f5e6ee389fe729459f727c2e4441
k.k <nrdy999902@gmail.com>
2021-04-15 01:18:32 +0900

    Add Employee new

--- src/controllers/toppage/TopPageIndexServlet.java
+++ src/controllers/toppage/TopPageIndexServlet.java
@@ -21,15 +21,12 @@ public class TopPageIndexServlet extends HttpServlet {
      */
     public TopPageIndexServlet() {
         super();
-
     }
 
     /**
-     * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
-     *      response)
+     * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
      */
-    protected void doGet(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/topPage/index.jsp");
         rd.forward(request, response);
     }
--- src/models/Employee.java
+++ src/models/Employee.java
@@ -107,5 +107,4 @@ public class Employee {
     public void setDelete_flag(Integer delete_flag) {
         this.delete_flag = delete_flag;
     }
-
 }
