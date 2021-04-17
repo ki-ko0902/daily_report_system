a88431baab65bb07d75ce639c50f5584607872a7
k.k <nrdy999902@gmail.com>
2021-04-17 20:31:21 +0900

    Modify top page

--- src/controllers/reports/ReportsCreateServlet.java
+++ src/controllers/reports/ReportsCreateServlet.java
@@ -33,22 +33,20 @@ public class ReportsCreateServlet extends HttpServlet {
     }
 
     /**
-     * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
-     *      response)
+     * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
      */
-    protected void doPost(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         String _token = request.getParameter("_token");
-        if (_token != null && _token.equals(request.getSession().getId())) {
+        if(_token != null && _token.equals(request.getSession().getId())) {
             EntityManager em = DBUtil.createEntityManager();
 
             Report r = new Report();
 
-            r.setEmployee((Employee) request.getSession().getAttribute("login_employee"));
+            r.setEmployee((Employee)request.getSession().getAttribute("login_employee"));
 
             Date report_date = new Date(System.currentTimeMillis());
             String rd_str = request.getParameter("report_date");
-            if (rd_str != null && !rd_str.equals("")) {
+            if(rd_str != null && !rd_str.equals("")) {
                 report_date = Date.valueOf(request.getParameter("report_date"));
             }
             r.setReport_date(report_date);
@@ -61,7 +59,7 @@ public class ReportsCreateServlet extends HttpServlet {
             r.setUpdated_at(currentTime);
 
             List<String> errors = ReportValidator.validate(r);
-            if (errors.size() > 0) {
+            if(errors.size() > 0) {
                 em.close();
 
                 request.setAttribute("_token", request.getSession().getId());
@@ -81,4 +79,5 @@ public class ReportsCreateServlet extends HttpServlet {
             }
         }
     }
+
 }
